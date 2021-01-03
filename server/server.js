71bb7297a8d649b273205a7e9ebbdb4a23a492b8
Anna Morris <annapatton444@gmail.com>
2021-01-03 17:16:33 -0600

    install nodemon

--- server/server.js
+++ server/server.js
@@ -1,9 +1,20 @@
 const express = require("express")
 
 const server = express();
+server.use(express.json())
 
+users = [] 
+
+// server.use('/', (req, res) => {
+//     res.send("api operational")
+// })
 server.post('/register', (req, res) => {
-    res.status(201).send("Hello World")});
+    console.log(req.body)
+    const user = req.body.username
+    users.push(user)
+    res.status(201).send(`Hello, ${user}`)
+});
 
-server.listen(5000, () => { 
-    console.log("Server running on http://localhost:5000") })
+server.listen(5000, () => {
+    console.log("Server running on http://localhost:5000")
+})
